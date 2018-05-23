1a8488e2be1abbc7278fd73d37ac9bbed59c8b04
George Schena <gs14aaa@herts.ac.uk>
2018-05-23 17:24:36 +0100

    altered footer & account support controller & view

---
+++ app/controllers/account_support_controller.rb
@@ -0,0 +1,2 @@
+class AccountSupportController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'account_support/show'
+
   resources :jobs
   devise_for :users
   root to: 'jobs#index'
-
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
